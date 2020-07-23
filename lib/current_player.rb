fd8993e726b547cbfb41fb836614ba81fc9e3caa
Eli Cusic <tntitans31@gmail.com>
2020-07-23 20:33:52 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,6 +1,6 @@
 def turn_count(board)
   counter = 0
-  board.each{|item| item == "X" || item == "O" ? counter += 1}
+  board.each{|item| item == "X" || item == "O" ? counter += 1 : }
   # do |x|
   #   if x == "X" || x == "O"
   #     counter += 1
